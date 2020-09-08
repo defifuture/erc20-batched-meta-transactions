e336cda7bd40c2625c7d86075520a4d24403252c
defifuture <67232250+defifuture@users.noreply.github.com>
2020-09-08 16:38:13 +0200

    docs + a test for sending ETH between addresses

---
+++ docs/truffle.md
@@ -0,0 +1,13 @@
+# How to run this project locally with Truffle
+
+## Compile contracts
+
+    truffle compile
+
+## Deploy contract to a local testnet (migrate)
+
+    truffle migrate
+
+## Testing
+
+    truffle test
--- test/erc20metabatch.js
+++ test/erc20metabatch.js
@@ -1,9 +1,47 @@
 const ERC20MetaBatch = artifacts.require("ERC20MetaBatch");
 
 contract("ERC20MetaBatch", async accounts => {
+
+    // test the name() function
     it("should return token name", async () => {
         let instance = await ERC20MetaBatch.deployed();
         let name = await instance.name();
+        console.log("Contract name: " + name);
         assert.equal(name, "Meta Tx Token");
     });
+
+    // send some ether to another address
+    it("should make the contract deployer send some ETH to another address", async() => {
+        let sender = accounts[0];
+        let receiver = accounts[1];
+        let amount = web3.utils.toWei("1", 'ether')
+
+        console.log("Sender: " + sender);
+        console.log("Receiver: " + receiver);
+        console.log("Amount: " + amount + " WEI");
+
+        let balanceSender = await web3.eth.getBalance(sender);
+        console.log("Sender balance: " + balanceSender);
+
+        let balanceReceiverBefore = await web3.eth.getBalance(receiver);
+        console.log("Receiver balance (before): " + balanceReceiverBefore);
+
+        let tx = {
+            from: sender,
+            to: receiver,
+            value: amount,
+            gasPrice: web3.utils.toBN(20000000000),
+            gas: web3.utils.toBN(6721975)
+        };
+
+        let result = await web3.eth.sendTransaction(tx);
+        // console.log(result);
+
+        let balanceReceiverAfter = await web3.eth.getBalance(receiver);
+        console.log("Receiver balance (after): " + balanceReceiverAfter);
+
+        // assert that the receiver's balance has increased by 1 ETH
+        assert.equal(amount, balanceReceiverAfter-balanceReceiverBefore);
+    });
+
 });
